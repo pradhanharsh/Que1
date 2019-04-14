e9fd596071e5a95638a055f4883bbb5c33ccc01e
pradhanharsh <49016696+pradhanharsh@users.noreply.github.com>
2019-04-14 22:41:13 +0530

    Add files via upload

---
+++ Untitled3.cpp
@@ -0,0 +1,15 @@
+#include<iostream> 
+using namespace std; 
+int pro = 5; 
+int process[5] = {0, 1, 2, 3, 4};
+int available[4] = {1,5,2,0}; 
+int res = 4; 
+int main() 
+{ 
+ 
+	int maxm[][res] = {{0,0,1,2}, 
+					{1,7,5,0}, 
+					{2,3,5,6}, 
+					{0,6,5,2}, 
+					{0,6,5,6}}; 
+}
